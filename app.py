14ce5ff6097e2647853193d27bebd3300805328b
pires14 <joao.bernardo.pires@escola.pr.gov.br>
2026-06-02 08:12:43 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+# Import do framework flask
+# Import do render_template para ler o HTML
+# request para capturar os dados
+from flask import Flask, render_template, request
+
+import mysql.connector 
+
+# Cria conexão
+bd_config = {
+    'host': '127.0.0.1',
+    'user': 'root',
+    'password': 'escola',
+    'database': 'cadastro1'
+}
+
+# Biblioteca mysql.connector conecta o Python com o MySQL
