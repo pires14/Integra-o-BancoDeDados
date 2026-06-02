5ffa9de18125f81a48fbe6301ec893a9e352e674
pires14 <joao.bernardo.pires@escola.pr.gov.br>
2026-06-02 08:13:26 -0300

    Update comment for MySQL connection in app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template, request
 
 import mysql.connector 
 
-# Cria conexão
+# Cria conexão com o mySQL
 bd_config = {
     'host': '127.0.0.1',
     'user': 'root',
