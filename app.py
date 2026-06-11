2c87b5bcf5dad814c8d4974872f04e663ccfcf00
pires14 <joao.bernardo.pires@escola.pr.gov.br>
2026-06-11 09:51:14 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 # Import do framework flask
-# Import do render_template para ler o HTML
+# Import do render_template para ler o HTML e busca ou o endereço do arquivo ou a URL
 # request para capturar os dados
 from flask import Flask, render_template, request
 
 import mysql.connector 
+
+# Para vincular as páginas e saberem ond estão:
+
+app = Flask(__name__)
 
 # Cria conexão com o mySQL
 bd_config = {
@@ -13,4 +17,13 @@ bd_config = {
     'database': 'cadastro1'
 }
 
+# Criação de rota para o arquivo HTML principal
+
+@app.route('/')
+
+def indexRota():
+    return render_template('index.html')
+
+@app.route('/cadastrar', methods=['POST'])
 # Biblioteca mysql.connector conecta o Python com o MySQL
+# decorador tem @
